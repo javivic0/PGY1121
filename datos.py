2f18d278298f6f0adf85b0b599f97045c7297b18
javivic0 <134542850+javivic0@users.noreply.github.com>
2023-05-24 14:34:01 -0400

    archivo datos.py sin validar

---
+++ datos.py
@@ -0,0 +1,9 @@
+#ejemplo de uso repositorio 
+
+print("datos personales")
+print("----------------")
+nom=str(input("ingrese su nombre: "))
+edad=int(input("ingrese su edad: "))
+print("-----------------------------")
+print(f"su nombre es {nom}")
+print(f"su edad es {edad}")
